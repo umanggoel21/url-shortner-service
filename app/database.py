ff048c7f9ad0aac4ad78bc87f1f3497e4576e1c4
umanggoel21 <172841812+umanggoel21@users.noreply.github.com>
2026-08-11 09:13:31 +0530

    configure database and resdis url via enviroment variables

--- app/database.py
+++ app/database.py
@@ -1,12 +1,16 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import redis
 
+load_dotenv()
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/urlshortener"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/urlshortener")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-import redis
 
-redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
